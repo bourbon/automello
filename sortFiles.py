6f55e904a6447aaf81d702e243737ba553cd73cc
Colin Raffel <craffel@gmail.com>
2012-02-11 21:17:14 -0800

    Added comments, some diagnostic info, removed scipy dependency

--- sortFiles.py
+++ sortFiles.py
@@ -10,62 +10,99 @@ import shutil
 import numpy as np
 
 class fileSorter:
-  def __init__( self, snippetDirectory, destinationDirectory, fs = 44100, nNotes = 60, baseNote = 24 ):
+  def __init__( self, snippetDirectory, destinationDirectory, fs = 44100, nNotes = 72, baseNote = 24 ):
+    # Store input params
     self.snippetDirectory = snippetDirectory
     self.destinationDirectory = destinationDirectory
     self.fs = fs
     self.nNotes = nNotes
     self.baseNote = baseNote
+    # Create the destination dir if it doesn't exist
+    if not os.path.exists( self.destinationDirectory ):
+      os.makedirs( self.destinationDirectory )
+    # Sort the files and copy them out
     self.sortFiles()
-    
+  
   def sortFiles( self ):
+    # Get list of wav files, recursively
     fileList = utility.getFiles( self.snippetDirectory, '.wav' )
+    # Array of per-file, per-note tonalities.  Each file gets a score for each pitch
     tonalities = np.zeros( ( len( fileList ), self.nNotes ) )
-    for n in np.arange( len( fileList ) ):
-      print (n*1.0)/len( fileList )
+    print "Getting tonalities..."
+    for n in np.arange( len( fileList ) ):      
+      # Get audio data
       audioData, fs = utility.getWavData( fileList[n] )
+      # Make sure the sampling rate matches...
       assert fs == self.fs
+      # Get the tonality scores for all notes for this file
       tonalities[n] = self.getTonality( audioData )
+    # Hash for the fundamental frequencies of each file
     fileFrequencies = {}
+    # Find the best file for each note
     for n in np.arange( self.nNotes ):
+      # Get the tonality scores
       tonalitiesForThisNote = tonalities[:,n]
+      # Get the sorted indices of tonality scores
       tonalitiesSort = np.argsort( tonalitiesForThisNote )[::-1]
+      # Keep track of which sorted array index we're getting the frequency for
       sortedIndex = 0
+      # What frequency is the note we're looking for?
       targetHz = utility.midiToHz( self.baseNote + n )
+      # What's the detected Hz of the note?
       detectedHz = 1.0
+      # Until we find an audio file whose YIN detected pitch is sufficiently close
       while ((targetHz/detectedHz) < .95 or (targetHz/detectedHz) > 1.05) and sortedIndex < tonalitiesSort.shape[0] and sortedIndex < 10:
+        # If this file has not been YIN analyzed yet, analyze it
         if not fileFrequencies.has_key( tonalitiesSort[sortedIndex] ):        
           audioData, fs = utility.getWavData( fileList[tonalitiesSort[sortedIndex]] )
+          # ... and store it so that you don't have to calculate it next time
           fileFrequencies[tonalitiesSort[sortedIndex]] = self.yinPitchDetect( audioData  )
         detectedHz = fileFrequencies[tonalitiesSort[sortedIndex]]
-        print targetHz, detectedHz
+        # Check the next file next time
         sortedIndex += 1
+      # If we didn't run out of files, copy out the file that we found (with the closest pitch)
       if sortedIndex < len( fileList ):
         shutil.copy( fileList[tonalitiesSort[sortedIndex-1]], os.path.join( self.destinationDirectory, str(n + self.baseNote) + ".wav" ) )
       else:
         shutil.copy( fileList[tonalitiesSort[0]], os.path.join( self.destinationDirectory, str(n + self.baseNote) + ".wav" ) )
-      
+  
+  # Get the tonality score for some audio data
   def getTonality( self, audioData ):
+    # Number of samples in the audio data
     N = audioData.shape[0]
+    # Tonality scores for each note
     tonalityScores = np.zeros( self.nNotes )
+    # Get magnitude spectrum for this audio data
     AudioData = np.abs( np.fft.rfft( audioData ) )
+    # Calculate spectral crest factor and RMS... not sure if we should use these
     spectralCrestFactor = np.max( AudioData )/np.mean( AudioData )
     RMS = np.sqrt( np.sum( audioData**2.0 ) )/(N*1.0)
+    # For each note
     for note in np.arange( self.nNotes ):
+      # Create a mask for only bins which are harmonics of the note in question
       mask = self.createMask( N, utility.midiToHz( self.baseNote + note ) )
+      # Calculate "monophonic tonality" - max of bins corresponding to harmonics/spectral mean
       monophonicTonality = np.max( AudioData[mask == 1] )/np.mean( AudioData[mask == 0] )
+      # Write out tonality score
       tonalityScores[note] = monophonicTonality*RMS
     return tonalityScores
-      
+  
+  # Create an array of 1s and 0s with 1s at harmonic multiples of the base frequency
   def createMask( self, N, baseFrequency ):
+    # Number of bins in the mask
     nBins = N/2 - 1
+    # Maximum frequency that we want to create a mask for
     maxFrequency = utility.midiToHz( self.baseNote + self.nNotes + 24 )
+    # Make sure it's not out of range... above nyquist or so
     if maxFrequency > (self.fs*.9)/2.0:
       maxFrequency = (self.fs*.9)/2.0
+    # Create the mask
     mask = np.zeros( nBins )
+    # Over all harmonic frequencies
     for harmonic in baseFrequency*(2**np.arange( 4 )):
       if harmonic > maxFrequency:
         break
+      # Set bins near this harmonic to 1
       mask[utility.hzToBins( harmonic, N, self.fs )] = 1
     return mask
 
--- utility.py
+++ utility.py
@@ -4,37 +4,29 @@
 # Created by Colin Raffel on 10/7/10
 
 import numpy as np
-import scipy.io.wavfile as wavfile
+import wave
 import csv
 import os
-import scipy.signal as signal
+import struct
 
 def getWavData( wavFile ):
   # Get wav data
-  fs, audioData = wavfile.read(wavFile)
-  # Convert to mono
-  if (len(audioData.shape) > 1) and (audioData.shape[1] > 1):
-    audioData = np.mean( audioData, axis=1 )
+  wav = wave.open (wavFile, "r")
+  (nChannels, sampleWidth, frameRate, nFrames, compressionType, compressionName) = wav.getparams()
+  frames = wav.readframes( nFrames*nChannels )
+  out = struct.unpack_from("%dh" % nFrames*nChannels, frames )
+  
+  # Convert 2 channles to numpy arrays
+  if nChannels == 2:
+    left = np.array( list( [out[i] for i in range( 0, len( out ), 2 )] ) )
+    right = np.array( list( [out[i] for i in range( 1, len( out ), 2 )] ) )
+  else:
+    left = np.array( out )
+    right = left
+  audioData = (left + right)/2.0
   # Normalize
   audioData = (32767.0*audioData)/np.max(np.abs(audioData))
-  return audioData, fs
-
-# Returns a list of annotations in a text file  
-def getBeats( file ):
-  # Try loading the beats
-  try:
-    beats = np.genfromtxt( file )
-  # Numpy couldn't load
-  except:
-    print "Error reading the file, returning empty beat list"
-    return np.array([])
-  # Some annotation files have two columns
-  if beats.ndim == 2:
-    beats = beats[:,1]
-  # Some annotation files have "end" at the end
-  if np.isnan( beats[-1] ):
-    beats = beats[:-1]
-  return beats
+  return audioData, frameRate
 
 # Get files of a certain type in a directory, recursively
 def getFiles( path, extension ):
@@ -54,103 +46,18 @@ def getSubdirectories( path ):
       dirList.append( os.path.join( root, dir ) )
   return dirList
 
-# Get annotation file paths/names
-def getAnnotations( directory ):
-  namePrefix = os.path.split( directory )[1]
-  rawPath = os.path.join( directory, namePrefix + "-footraw.txt" )
-  predictPath = os.path.join( directory, namePrefix + "-footpredict.txt" )
-  handCuesPath = os.path.join( directory, namePrefix + "-handcues.txt" )
-  return getBeats( rawPath ), getBeats( predictPath ), getBeats( handCuesPath )
-
-# Get tempo and confidence
-def getTempoFromAnnotations( beats ):
-  # Compute inner-beat-intervals
-  innerBeatIntervals = np.diff( beats )
-  # Mean -> tempo
-  tempo = 60.0/np.mean( innerBeatIntervals )
-  return tempo
-
-def toMono( audioData ):
-  # For now, sum left and right channels
-  audioData = (audioData[::2] + audioData[1::2])/2.0
-  return audioData
-  
-# Down sample a signal by taking the mean over frames
-def downsampleMean( data, frameSize ):
-  nFrames = np.floor(data.shape[0]/frameSize)
-  dataDownsampled = np.zeros(nFrames)
-  for n in np.arange(nFrames):
-    dataDownsampled[n] = np.mean(data[n*frameSize:(n+1)*frameSize])
-  return dataDownsampled
-
-def downsampleMax( data, frameSize ):
-  nFrames = np.floor(data.shape[0]/frameSize)
-  dataDownsampled = np.zeros(nFrames+1)
-  #for n in np.arange(nFrames):
-  #  currentMax = np.max(data[n*frameSize:(n+1)*frameSize])
-  #  currentMin = np.min(data[n*frameSize:(n+1)*frameSize])
-  #  if currentMax > np.abs(currentMin):
-  #    dataDownsampled[n] = currentMax
-  #  else:
-  #    dataDownsampled[n] = currentMin
-  for n in np.arange(nFrames):
-    dataDownsampled[n] = np.max(np.abs(data[n*frameSize:(n+1)*frameSize]))
-
-  if data.shape[0] > nFrames*frameSize:
-    dataDownsampled[nFrames] = np.max(data[nFrames*frameSize:])
-  
-  alternate = np.tile(np.array([1, -1]), dataDownsampled.shape[0]/2)
-  if alternate.shape[0] < dataDownsampled.shape[0]:
-    alternate = np.append(alternate, np.array([1]))
-  dataDownsampled = dataDownsampled*alternate
-  return dataDownsampled
-  
-# Calculate geometric mean for lage arrays of large values
-def geometricMean( data ):
-  N = float(data.shape[0])
-  largestValue = np.max( data )
-  frameSize = 2
-  nFrames = np.floor(N/2)
-  geometricMean = 1.0
-  for n in np.arange( nFrames ):
-    geometricMean = geometricMean * np.power( data[2*n]*data[2*n+1], 1/N )
-  if np.floor(N/2) != N/2:
-    geometricMean = geometricMean * np.power( data[-1], 1/N )
-  return geometricMean
-
-# Split a signal into frames
-def splitSignal( x, hop, frameSize ):
-  nFrames = np.floor( (x.shape[0]-frameSize)/(1.0*hop) )
-  # Pre-allocate matrix
-  xSplit = np.zeros( (nFrames, frameSize) )
-  for n in np.arange(nFrames):
-    xSplit[n] = x[n*hop:n*hop+frameSize]
-  return xSplit
-
-# Parabolic interpolation
-def parabolicInterpolate( signal, i ):
-  # Make sure we aren't out of boundsi
-  if i > 0 and i < signal.shape[0] - 1:
-    offset = (signal[i+1] - signal[i-1])/(2.0*(2.0*signal[i] - signal[i+1] - signal[i-1]))
-  # Otherwrise don't calculate offset
-  else:
-    offset = 0
-  return i + offset
-
-def roundArray( input ):
-  output = "["
-  for value in input:
-    output += "%.3f, " % value
-  output =  output[:-2] + "]"
-  return output
-
+# Convert midi note to Hz
 def midiToHz( midiNote ):
   return 440.0*(2.0**((midiNote - 69)/12.0))
 
+# Convert bin in FFT to Hz
 def binToHz( bin, N, fs ):
   return fs*bin/(N*1.0)
 
+# Return bins in an FFT which are close to a Hz value
 def hzToBins( hz, N, fs, tolerance = 0.02 ):
+  # Range near bins in tolerance range
   binRange = np.arange( (1.0 - tolerance)*hz*N/fs, (1.0 + tolerance)*hz*N/fs )
+  # Convert arange to integer indices
   bins = np.array( np.round( binRange ), dtype = np.int )
   return bins
